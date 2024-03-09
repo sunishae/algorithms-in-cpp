d2289f801ec5ca11aef1b94c54c8e0dbb7463148
Sunishae <130846629+sunishae@users.noreply.github.com>
2024-03-09 23:49:14 +0900

    [Bronze V] Title: A+B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.cc
@@ -0,0 +1,16 @@
+// number : 1000
+// description : A+B
+// date : 2024.03.09//
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int a =0;
+    int b =0;
+    cin >> a >> b;
+    cout << a + b << endl;
+
+    return 0;
+}
