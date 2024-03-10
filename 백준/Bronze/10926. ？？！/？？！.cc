e4871bbcf3f68c6ac56dcbc0f0dc266c2f7e62b4
Sunishae <130846629+sunishae@users.noreply.github.com>
2024-03-10 16:56:11 +0900

    [Bronze V] Title: ??!, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.cc
@@ -0,0 +1,16 @@
+// number : 10926
+// description : !!? 출력
+// date : 2024.03.10
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string id;
+    getline(cin, id);
+
+    cout << id + "??!" << endl;
+
+    return 0;
+}
