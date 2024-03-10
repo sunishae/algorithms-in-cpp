5bc91688c0392d978340d50d8c68aacea754efc2
Sunishae <130846629+sunishae@users.noreply.github.com>
2024-03-10 17:26:41 +0900

    [Bronze V] Title: 개, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.cc
@@ -0,0 +1,16 @@
+// number : 10172
+// description : 개 그리기
+// date : 2024.03.10
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "|\\_/|" << endl;
+    cout << "|q p|   /}" << endl;
+    cout << "( 0 )\"\"\"\\" << endl;
+    cout << "|\"^\"`    |" << endl;
+    cout << "||_/=\\\\__|" << endl;
+
+    return 0;
+}
