c9bf7fb7fee81ed0d14591ce8fe415fb22ab2e18
Sunishae <130846629+sunishae@users.noreply.github.com>
2024-03-10 16:58:51 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.cc
@@ -0,0 +1,14 @@
+// number : 18108
+// description : 불기 연도를 서기 연도로 변환
+// date : 2024.03.10
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int year;
+    cin >> year;
+    cout << year - 543 << endl;
+    
+    return 0;
+}
