983ee404d574f3c395cebc1690d0129ffbdf1c2f
Sunishae <130846629+sunishae@users.noreply.github.com>
2024-03-10 16:46:38 +0900

    [Bronze V] Title: 사칙연산, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.cc
@@ -0,0 +1,20 @@
+// number : 10869
+// description : 사칙연산 모두 계산
+// date : 2024.03.10
+
+#include <iostream>
+ 
+using namespace std;
+int main(int argc, char const *argv[]) {
+ 
+    int a, b;
+    cin >> a >> b;
+
+    cout << a + b << endl;
+    cout << a - b << endl;
+    cout << a * b << endl;
+    cout << a / b << endl;
+    cout << a % b << endl;
+    
+    return 0;
+}
