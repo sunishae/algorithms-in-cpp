09db646acecc80a298947c3c212494958b01a7f8
Sunishae <130846629+sunishae@users.noreply.github.com>
2024-03-10 17:23:37 +0900

    [Bronze V] Title: 고양이, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.cc
@@ -0,0 +1,15 @@
+// number : 10171
+// description : 고양이 그리기
+// date : 2024.03.10
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "\\    /\\" << endl;
+    cout << " )  ( ')"<< endl;
+    cout << "(  /  )"<< endl;
+    cout << " \\(__)|"<< endl;
+
+    return 0;
+}
