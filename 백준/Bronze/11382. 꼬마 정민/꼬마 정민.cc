4df6458b5869b28e532ea425fa559bc849ac3c3e
Sunishae <130846629+sunishae@users.noreply.github.com>
2024-03-10 17:18:46 +0900

    [Bronze V] Title: 꼬마 정민, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.cc
@@ -0,0 +1,15 @@
+// number : 11382
+// description : A + B + C
+// date : 2024.03.10
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    long long A, B, C;
+    cin >> A >> B >> C;
+    cout << A + B + C << endl;
+
+    return 0;
+}
