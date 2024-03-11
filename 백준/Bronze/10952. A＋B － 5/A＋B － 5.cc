b2daca1ecb6e2d5fce848885764f3dc310969615
Sunishae <130846629+sunishae@users.noreply.github.com>
2024-03-11 21:04:20 +0900

    [Bronze V] Title: A+B - 5, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.cc
@@ -0,0 +1,21 @@
+// number : 10952
+// description : A+B
+// date : 2024.03.11
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int a,b;
+
+    while(true){
+        cin >> a >> b;
+        if(a == 0 && b == 0) break;
+        cout << a+b << '\n';
+    }
+}
