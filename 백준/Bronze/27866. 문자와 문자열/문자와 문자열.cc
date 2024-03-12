f7c5a17663452b072d897c8fd7a28ca3d2ddd6ba
Sunishae <130846629+sunishae@users.noreply.github.com>
2024-03-12 23:00:29 +0900

    [Bronze V] Title: 문자와 문자열, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.cc
@@ -0,0 +1,20 @@
+// number : 27866
+// description : 문자열 인덱스 반환
+// date : 2024.03.12
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    string str;
+    cin >> str;
+
+    int i;
+    cin >> i;
+
+    cout << str[i-1] << endl;
+
+    return 0;
+}
