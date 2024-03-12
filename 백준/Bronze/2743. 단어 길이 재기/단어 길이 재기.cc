0d77628761ef744affff99daaf8779f11e658521
Sunishae <130846629+sunishae@users.noreply.github.com>
2024-03-12 23:17:28 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.cc
@@ -0,0 +1,19 @@
+// number : 27866
+// description : 문자열 인덱스 반환
+// date : 2024.03.12
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    string str;
+    getline(cin, str);
+
+    cout << str.length() << endl;
+
+
+
+    return 0;
+}
