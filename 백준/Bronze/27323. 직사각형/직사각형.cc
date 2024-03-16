b9397e4c5496c09752f5e9c03d837f36f3a07460
Sunishae <130846629+sunishae@users.noreply.github.com>
2024-03-16 18:17:56 +0900

    [Bronze V] Title: 직사각형, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/27323. 직사각형/직사각형.cc
@@ -0,0 +1,20 @@
+// number : 27323
+// description : 직사각형의 넓이
+// date : 2024.03.16
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int A,B;
+    cin >>A;
+    cin >>B;
+
+    cout << A * B << '\n';
+
+    return 0;
+}
