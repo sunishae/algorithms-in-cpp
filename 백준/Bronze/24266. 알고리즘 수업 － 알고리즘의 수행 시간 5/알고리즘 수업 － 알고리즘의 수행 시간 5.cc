ba55d14868b9d1df72a07de765562680978aac9d
Sunishae <130846629+sunishae@users.noreply.github.com>
2024-03-16 23:26:24 +0900

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 5, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24266. 알고리즘 수업 － 알고리즘의 수행 시간 5/알고리즘 수업 － 알고리즘의 수행 시간 5.cc
@@ -0,0 +1,17 @@
+// number : 
+// description : 시간복잡도
+// date : 2024.03.16
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    long long N;
+    cin >> N;
+
+    cout << N * N * N << '\n' << 3;
+
+    return 0;
+}
