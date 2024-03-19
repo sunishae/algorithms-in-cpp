1e9877b13eb46fd5fa842c9c7b99b78c5bf51c38
Sunishae <130846629+sunishae@users.noreply.github.com>
2024-03-19 14:37:39 +0900

    [Bronze II] Title: 대표값2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2587. 대표값2/대표값2.cc
@@ -0,0 +1,24 @@
+// number : 2587
+// description : 평균과 중앙값
+// date : 2024.03.19
+
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main(){
+
+    int arr[5];
+    int sum = 0;
+    for(int i = 0; i < 5; i++){
+        cin >> arr[i];
+        sum += arr[i];
+    }
+
+    sort(arr, arr + 5);
+    cout << sum/5 << endl;
+    cout << arr[2] << endl;
+
+    return 0;
+}
